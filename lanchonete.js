c012f94e677d4032d22635cd513eed87ef6cddd6
jao006 <joaomelquesedequecalaca95@gmail.com>
2025-04-14 17:35:38 -0300

    commit 1

---
+++ lanchonete.js
@@ -0,0 +1,37 @@
+function calcular() {
+    let codigo = Number(document.getElementById("codigo").value);
+    let quantidade = Number(document.getElementById("quantidade").value);
+    let texto = document.getElementById("texto");
+    let total = 0;
+
+    // Testando os códigos dos lanches 
+    switch (codigo) {
+        case 1:
+            total = quantidade * 18.00; // X-Calabresa
+            break;
+        case 2:
+            total = quantidade * 15.00; // X-Salamimho
+            break;
+        case 3:
+            total = quantidade * 19.00; // X-Frango
+            break;
+        case 4:
+            total = quantidade * 12.00; // X-Salada
+            break;
+        case 5:
+            total = quantidade * 15.00; // X-Burguer
+            break;
+        case 6:
+            total = quantidade * 35.00; // A moda da casa
+            break;
+        case 7:
+            total = quantidade * 22.00; // X-Gourmet
+            break;
+        default:
+            texto.innerHTML = 'Código inválido'; // Mensagem de erro
+            return; // Sai da função se o código for inválido
+    }
+
+    // Exibe o total formatado com duas casas decimais
+    texto.innerHTML = "R$ " + total.toFixed(2);
+}
