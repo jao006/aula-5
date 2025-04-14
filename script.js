2fab735ad5a4da3fd99390d393b6aacaac7cc5ab
jao006 <joaomelquesedequecalaca95@gmail.com>
2025-04-14 17:47:08 -0300

    commit 4

--- script.js
+++ script.js
@@ -10,22 +10,31 @@ function calcular() {
             total = quantidade * 18.00; // X-Calabresa
             break;
         case 2:
-            total = quantidade * 15.00; // X-Salamimho
+            total = quantidade * 25.00; // X-Salamimho
             break;
         case 3:
-            total = quantidade * 19.00; // X-Frango
+            total = quantidade * 21.00; // X-Frango
             break;
         case 4:
-            total = quantidade * 12.00; // X-Salada
+            total = quantidade * 15.00; // X-Salada
             break;
         case 5:
-            total = quantidade * 15.00; // X-Burguer
+            total = quantidade * 17.00; // X-Burguer
             break;
         case 6:
             total = quantidade * 35.00; // A moda da casa
             break;
         case 7:
-            total = quantidade * 22.00; // X-Gourmet
+            total = quantidade * 44.00; // X-Gourmet
+            break;
+        case 8:
+            total = quantidade * 55.00; // X-Gourmet
+            break;
+        case 9:
+            total = quantidade * 44.00; // X-Gourmet
+            break;
+        case 10:
+            total = quantidade * 144.00; // X-Gourmet
             break;
         default:
             texto.innerHTML = 'Código inválido'; // Mensagem de erro
